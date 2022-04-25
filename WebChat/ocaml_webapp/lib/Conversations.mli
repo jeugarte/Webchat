8183115c5433da35c0dbe922670d77d4bee6d8e0
jeu9 <jeu9@cornell.edu>
2022-04-24 21:51:45 -0400

    First implementation of a contacts query system

---
+++ WebChat/ocaml_webapp/lib/Conversations.mli
@@ -0,0 +1,17 @@
+type conversation = {
+  conversation_id : int;
+  conversation_name : int;
+  creator_id : int
+}
+
+val migrate : unit -> (unit, 'a) result Lwt.t
+
+val rollback : unit -> (unit, 'a) result Lwt.t
+
+val insert_convo : string -> string -> unit -> (unit, 'a) result Lwt.t
+
+val get_convo_name_from_id :  int -> unit -> (string, 'a) result Lwt.t
+
+val get_creator_from_id :  int -> unit -> (string, 'a) result Lwt.t
+
+(*val read_all : unit -> (conversation list, 'a) result Lwt.t*)
