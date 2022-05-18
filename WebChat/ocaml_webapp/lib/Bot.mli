77aacfb979686839d5500cc7e76820ceeb896347
Stanley Jiang <sj466@cornell.edu>
2022-05-18 01:06:03 -0400

    add documentation for bots

--- WebChat/ocaml_webapp/lib/Bot.mli
+++ WebChat/ocaml_webapp/lib/Bot.mli
@@ -1,2 +1,13 @@
 val bob_bot_response : string -> int -> bool * string
+(** [bob_bot_response] takes in a string [s] and an int [i] and returns
+    a tuple [(b,response)] where [b] is a boolean representing if
+    bob-bot will respond and [response] is the string representing the
+    response to the input string [s]. [i] is a int used purely for
+    testing randomness and is [0] to allow random behavior. **)
+
 val joe_bot_response : string -> int -> bool * string
+(** [joe_bot_response] takes in a string [s] and an int [i] and returns
+    a tuple [(b,response)] where [b] is a boolean representing if
+    joe-bot will respond and [response] is the string representing the
+    response to the input string [s]. [i] is a int included to match bot
+    response signatures **)
